2406a39391732c71d8a20198dfcaa8d703f3daa1
Daniel Renner <renner.da@gmail.com>
2014-03-24 23:40:20 +0100

    Fixed tests (adding rspec next). Created gallery overview view.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,7 +4,7 @@ class PicturesController < ApplicationController
   before_action :load_picture, only: :show
 
   def show
-    send_file Rails.root.join("02_Pictures", "slider", "P1010392.JPG"), type: "image/jpeg", disposition: "inline"
+    send_file Rails.root.join("02_Pictures", "Test", "thumbs", "test.JPG"), type: "image/jpeg", disposition: "inline"
   end
 
   private
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Wedding::Application.initialize!
+
+def in_memory_database?
+  ENV["RAILS_ENV"] == "test" and
+      ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter
+end
+
+if in_memory_database?
+  puts "creating sqlite in memory database"
+  load "#{Rails.root}/db/schema.rb" # use db agnostic schema by default
+  # ActiveRecord::Migrator.up('db/migrate') # use migrations
+end
